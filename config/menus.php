20a0e0c03c02b88597640d54fc2aed3feb607ae9
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-04-17 19:46:49 +0200

    mise en place fichier menus.php

---
+++ config/menus.php
@@ -0,0 +1,60 @@
+<?php
+
+return[
+    'entries' =>[
+        [
+            'id' => 0,
+            'name' => 'entree 1',
+            'price' => 2,
+            'qty' => 1
+        ],
+        [
+            'id' => 1,
+            'name' => 'entree 2',
+            'price' => 2,
+            'qty' => 1
+        ],
+    ],
+    'drinks' =>[
+        [
+            'id' => 2,
+            'name' => 'boisson 1',
+            'price' => 4.50,
+            'qty' => 1
+        ],
+        [
+            'id' => 3,
+            'name' => 'boisson 2',
+            'price' => 3.60,
+            'qty' => 1
+        ]
+    ],
+    'dishes' =>[
+        [
+            'id' => 4,
+            'name' => 'plat 1',
+            'price' => 14.50,
+            'qty' => 1
+        ],
+        [
+            'id' => 5,
+            'name' => 'plat 2',
+            'price' => 13.60,
+            'qty' => 1
+        ]
+    ],
+    'desserts' =>[
+        [
+            'id' => 6,
+            'name' => 'dessert 1',
+            'price' => 4.50,
+            'qty' => 1
+        ],
+        [
+            'id' => 7,
+            'name' => 'dessert 2',
+            'price' => 5.60,
+            'qty' => 1
+        ]
+    ]
+        ];
